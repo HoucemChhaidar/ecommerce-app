2af8b47fbb4b245e74f89f2dbf76dc520f81913e
MohamedJmal99 <Mohamedjmal73@gmail.com>
2024-03-24 05:30:41 +0100

    Finished the cart integration

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,8 +1,8 @@
-import { Component, Inject, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { Product } from '../../Models/Product/product';
-import { DOCUMENT } from '@angular/common';
+import { CartService } from '../../services/cart/cart.service';
 
 @Component({
   selector: 'app-card',
@@ -14,30 +14,11 @@ import { DOCUMENT } from '@angular/common';
 export class CardComponent {
   @Input() product!: Product;
   faShoppingCart = faShoppingCart;
-  localStorage?: Storage;
-  constructor(@Inject(DOCUMENT) document: Document) {
-    this.localStorage = document.defaultView?.localStorage;
-  }
+  constructor(private cartServices: CartService) {}
 
   addToCart(product: Product) {
-    try {
-      let cart: string | null | undefined = this.localStorage?.getItem('cart');
-      if (cart) {
-        const parsedCart = JSON.parse(cart);
-        if (!Array.isArray(parsedCart)) {
-          throw new Error('Cart data is not an array');
-        }
-        parsedCart.push(product.toJson());
-        cart = JSON.stringify(parsedCart);
-      } else {
-        cart = JSON.stringify([product.toJson()]);
-      }
-      this.localStorage?.setItem('cart', cart);
-      console.log(cart);
-    } catch (error) {
-      console.error('Error adding to cart:', error);
-    }
+    this.cartServices.addToCart(product);
   }
-  
-  
+
+
 }
